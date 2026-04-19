7a8cb8c30b584353ac60a8e0b4dd4caef31fb56d
Mahamed Elmi <mahamedelmi17@gmail.com>
2026-04-19 04:02:11 +0100

    Update 0.0.3
    Integrated pick-ups, developed the main weaponry behaviour.

--- Assets/FistsBehaviour.cs
+++ Assets/FistsBehaviour.cs
@@ -1,16 +1,81 @@
+using System;
 using UnityEngine;
 
 public class FistsBehaviour : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    private enum WeaponType {         
+        Fists,
+        Knife,
+        Hatchet
     }
 
-    // Update is called once per frame
+    [SerializeField] private WeaponType currentWeapon = WeaponType.Fists;
+    [SerializeField] private GameObject hand; 
+
+    void Start()
+    {
+
+        currentWeapon = WeaponType.Fists;
+    }
+
+ 
+    void Weapon()
+    {
+        switch (currentWeapon)
+        {
+              case WeaponType.Fists:
+                Fist();
+                break;
+            case WeaponType.Knife:
+                Knives();
+                break;
+            case WeaponType.Hatchet:
+                break;
+            default:
+                Debug.Log("No weapon selected. Switching to default.");
+                break;
+        }
+    }       
+     
+
+    void Fist()
+    {
+      if (gameObject.CompareTag("Fists"))
+      {
+        Debug.Log("Fists are active.");
+            return;
+        }
+
+    }
+
+  void Knives()
+    {
+        if (gameObject.CompareTag("Knife"))
+        {
+            Debug.Log("Knife is active.");
+            return;
+        }
+ 
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("KnifeCrate"))
+        {
+            Debug.Log("Knife crate hit. Switching to Knife.");
+            currentWeapon = WeaponType.Knife;
+            Destroy(collision.gameObject);
+        }
+        else if (collision.gameObject.CompareTag("HatchetCrate"))
+        {
+            Debug.Log("Hatchet crate hit. Switching to Hatchet.");
+           currentWeapon = WeaponType.Hatchet;
+            Destroy(collision.gameObject);
+        }
+
+    }
+
     void Update()
     {
-        
+        Weapon();
     }
 }
