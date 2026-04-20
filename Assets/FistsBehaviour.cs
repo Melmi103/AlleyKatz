d241989c29c8f7023e57d10b098ea00a6dabfea1
Mahamed Elmi <mahamedelmi17@gmail.com>
2026-04-20 03:11:00 +0100

    Update 0.0.4
    - Configured collisions between enemies and the player.
    - Fixed communication issues between a public event.
    - Configured weapon hit-ranges for most melee weaponry.
    - Identified each scripts purpose and management.
    - Going to sleep its 3:10 am. . . D:

--- Assets/FistsBehaviour.cs
+++ Assets/FistsBehaviour.cs
@@ -1,9 +1,11 @@
 using System;
 using UnityEngine;
+//This script manages the player's weapon state, sprites, animations, interactions with pickups and communicates weapon changes to other components.
+
 
 public class FistsBehaviour : MonoBehaviour
 {
-    private enum WeaponType {         
+    public enum WeaponType {         
         Fists,
         Knife,
         Hatchet
@@ -12,9 +14,22 @@ public class FistsBehaviour : MonoBehaviour
     [SerializeField] private WeaponType currentWeapon = WeaponType.Fists;
     [SerializeField] private GameObject hand; 
 
+    public event Action<WeaponType> OnWeaponChanged;
+
+    public WeaponType GetCurrentWeapon => currentWeapon;
+
+    private void SetWeapon(WeaponType newWeapon)
+    {
+           if (currentWeapon != newWeapon)
+        {
+            currentWeapon = newWeapon;
+            OnWeaponChanged?.Invoke(currentWeapon);
+        }
+    }
+
     void Start()
     {
-
+        Fist();
         currentWeapon = WeaponType.Fists;
     }
 
@@ -23,7 +38,7 @@ public class FistsBehaviour : MonoBehaviour
     {
         switch (currentWeapon)
         {
-              case WeaponType.Fists:
+            case WeaponType.Fists:
                 Fist();
                 break;
             case WeaponType.Knife:
@@ -35,47 +50,42 @@ public class FistsBehaviour : MonoBehaviour
                 Debug.Log("No weapon selected. Switching to default.");
                 break;
         }
-    }       
-     
+    }
 
     void Fist()
     {
-      if (gameObject.CompareTag("Fists"))
-      {
-        Debug.Log("Fists are active.");
-            return;
-        }
+
+            // Fist-specific behavior (Sprites, anims, etc)
+            SetWeapon(WeaponType.Fists);
 
     }
 
   void Knives()
     {
-        if (gameObject.CompareTag("Knife"))
-        {
-            Debug.Log("Knife is active.");
-            return;
-        }
- 
+      
+            
+        // Knife-specific behavior (Sprites, anims, etc)
+                SetWeapon(WeaponType.Knife);
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("KnifeCrate"))
         {
             Debug.Log("Knife crate hit. Switching to Knife.");
-            currentWeapon = WeaponType.Knife;
+            Knives();
             Destroy(collision.gameObject);
         }
         else if (collision.gameObject.CompareTag("HatchetCrate"))
         {
             Debug.Log("Hatchet crate hit. Switching to Hatchet.");
-           currentWeapon = WeaponType.Hatchet;
             Destroy(collision.gameObject);
         }
-
     }
 
     void Update()
     {
         Weapon();
+        //Debug.Log ("Current weapon in FistsBehaviour: " + currentWeapon);
     }
 }
